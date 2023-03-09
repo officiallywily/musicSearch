cb06bea5b1803c22f1d4dbae9bbc04377f667485
Wonseok Park <wpark920@gmail.com>
2023-03-08 16:14:33 -0800

    setup Babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,20 @@ module.exports = {
   output: {
     path:path.resolve(__dirname, "client/dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.?(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "client/src", "index.html"),
